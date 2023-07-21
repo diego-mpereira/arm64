9f1a6f36db6a5f41083def95985004ba6c38cb93
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-07-21 14:25:04 -0300

    Update rotacionar-tela-e-touch.sh

--- rotacionar-tela-e-touch.sh
+++ rotacionar-tela-e-touch.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-## Script de rotação de tela no início do sistema
+## Script de rotação de tela e touchscreen para linux.
 ## git @diego-mpereira VideoSoft
-
-## Ler modelo do TouchPad Instalado
+# Ler modelo do TouchPad Instalado
 installedTouch=$(xinput list --name-only | grep "Touch" | head -n 1)
-## Rotacionar Touchpad
+# Rotacionar Touchpad
 xinput set-prop "${installedTouch}" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
 # Ler nome do monitor ativo
 activeDisplay=$(xrandr | grep " connected " | awk '{ print$1 }')
-declare -a testeArr=($activeDisplay)
-for key in "${!testeArr[@]}"
-do
-	monitor=${testeArr[$key]}
-	display="monitor${monitor}"
- # Rotacionar tela para cada monitor ativo
-	xrandr --output $monitor --mode 1920x1080 --rotate right
-done
+# Rotacionar tela
+xrandr --output $activeDisplay --mode 1920x1080 --rotate right
