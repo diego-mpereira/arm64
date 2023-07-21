03f2ba9b82cce28d3e2232dde60f2795a0a21a41
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-07-21 14:23:21 -0300

    Update rotacionar-tela-e-touch.sh

--- rotacionar-tela-e-touch.sh
+++ rotacionar-tela-e-touch.sh
@@ -6,14 +6,13 @@
 installedTouch=$(xinput list --name-only | grep "Touch" | head -n 1)
 ## Rotacionar Touchpad
 xinput set-prop "${installedTouch}" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
-## Sleep
-sleep 2
+# Ler nome do monitor ativo
 activeDisplay=$(xrandr | grep " connected " | awk '{ print$1 }')
 declare -a testeArr=($activeDisplay)
 for key in "${!testeArr[@]}"
 do
 	monitor=${testeArr[$key]}
 	display="monitor${monitor}"
+ # Rotacionar tela para cada monitor ativo
 	xrandr --output $monitor --mode 1920x1080 --rotate right
-	xfconf-query --channel xfce4-desktop --property /backdrop/screen0/${display}/workspace0/last-image --set /opt/videosoft/scripts/image-install/videosoft-vertical.png
 done
