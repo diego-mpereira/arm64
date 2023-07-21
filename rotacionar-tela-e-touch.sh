1a18b3b7f04cd389a3a52e521f377657200717c0
Diego Meirinho C Pereira Gonçalves <137406825+diego-mpereira@users.noreply.github.com>
2023-07-21 15:52:05 -0300

    Update rotacionar-tela-e-touch.sh

--- rotacionar-tela-e-touch.sh
+++ rotacionar-tela-e-touch.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 ## Script de rotação de tela e touchscreen para linux.
 ## git @diego-mpereira VideoSoft
-echo -n "Orientação de rotação de tela desejada: 1-Right 2-Left 3-Normal 4-Inverted" 
+echo -n "Orientação de rotação de tela desejada: 
+1-Right 
+2-Left 
+3-Normal 
+4-Inverted 
+"
 read orientacao
-if [ -z $orientacao ]
-then
-      exit
-elseif [ $orientacao = 1 ]
-      matrix="0 1 0 -1 0 1 0 0 1"
-      rotscreen="right"
-elseif [ $orientacao = 2 ] 
-      matrix="0 -1 1 1 0 0 0 0 1"
-      rotscreen="left"
-elseif [ $orientacao = 3 ] 
-      matrix="1 0 0 0 1 0 0 0 1"
-      rotscreen="normal"
-elseif [ $orientacao = 4 ] 
-      matrix="-1 0 1 0 -1 1 0 0 1"
-      rotscreen="inverted"
+if [ "$orientacao" -eq "1" ]; then
+      matrix="0 1 0 -1 0 1 0 0 1";
+      rotscreen="right";
 fi
-echo $orientacao
+if [ "$orientacao" -eq "2" ];
+then
+      matrix="0 -1 1 1 0 0 0 0 1";
+      rotscreen="left";
+fi
+if [ "$orientacao" -eq "3" ];
+then
+      matrix="1 0 0 0 1 0 0 0 1";
+      rotscreen="normal";
+fi
+if [ $"$orientacao" -eq "4" ];
+then
+      matrix="-1 0 1 0 -1 1 0 0 1";
+      rotscreen="inverted";
+fi
 # Ler modelo do TouchPad Instalado
 installedTouch=$(xinput list --name-only | grep "Touch" | head -n 1)
+##installedTouch=\"${installedTouch}\"
+# Criar script temporario para Rotacionar Touchpad
+echo "xinput set-prop \"${installedTouch}\" \"Coordinate Transformation Matrix\" ${matrix}" >>/tmp/rotacionatouch.sh
 # Rotacionar Touchpad
-xinput set-prop "${installedTouch}" "Coordinate Transformation Matrix" ${matrix}
+bash /tmp/rotacionatouch.sh
+# Remover script temporario
+cat /tmp/rotacionatouch.sh
+rm /tmp/rotacionatouch.sh
 # Ler nome do monitor ativo
 activeDisplay=$(xrandr | grep " connected " | awk '{ print$1 }')
 # Rotacionar tela
-xrandr --output $activeDisplay --mode 1920x1080 --rotate ${rotscreen}
+xrandr --output $(echo $activeDisplay) --mode 1920x1080 --rotate ${rotscreen}
